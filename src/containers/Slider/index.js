8ac4d69770425d6e25c4b43bdad74070b416e0c1
SALHI Hanane <benkada.hanane@gmail.com>
2024-08-16 15:57:06 +0200

    odre décroissant

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,7 +8,8 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+//ordre décroissant
+    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
 
   const nextCard = () => {
